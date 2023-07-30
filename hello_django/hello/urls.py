87dd775b660f96f0e8142f65725108cdf040338e
David Stevenson <drstevenson1958@yahoo.com>
2023-07-30 16:44:13 -0400

    Use the code snippet to add pages

--- hello_django/hello/urls.py
+++ hello_django/hello/urls.py
@@ -4,6 +4,8 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
     path("hello/<name>", views.hello_there, name="hello_there"),
 ]
 
